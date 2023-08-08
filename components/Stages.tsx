e72830554fcfdba4f5d8d7e6ec16c9d2c94d60b6
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2023-08-08 09:32:28 -0400

    Update Stages.tsx

--- components/Stages.tsx
+++ components/Stages.tsx
@@ -50,7 +50,8 @@ export default function Stages() {
 			>
 				<LOD models={meshes} distances={doorwayDistances} />
 			</Float> */}
-			<RollingPin />
+			<LOD models={meshes} distances={doorwayDistances} />
+			{/* <RollingPin /> */}
 		</>
 	);
 }
